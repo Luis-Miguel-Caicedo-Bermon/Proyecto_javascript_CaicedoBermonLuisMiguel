40e2d6e1f87d4e5ca413c349fd37f34d19dda985
LuisMiguel <luismiguel110205@gmail.com>
2024-08-20 12:59:05 -0500

    feat :sparkles: init to code of JavaScript

---
+++ script.js
@@ -0,0 +1,434 @@
+var info = [
+    {
+        "grupos": [
+            {
+                "ruta": "nodeJS",
+                "modulos": {
+                    "fundamentos de programacion": [
+                        "introduccion a la algoritmia",
+                        "PSeint",
+                        "Python"
+                    ],
+                    "programacion web": [
+                        "HTML",
+                        "CSS",
+                        "booststrap"
+                    ],
+                    "programacion formal": "C#",
+                    "bases de datos": [
+                        "MongoDB",
+                        "Mysql"
+                    ],
+                    "backend": [
+                        "nodeJS",
+                        "Spring Boot"
+                    ]
+                },
+                "nombres": "t1",
+                "fecha de inicio": "20-01-2024",
+                "fecha de finalizacion": "20-11-2024",
+                "salon de entrenamiento": "aula1",
+                "grupo": [
+                    {
+                        "id": "1",
+                        "nombre": "yurley",
+                        "apellido": "botello",
+                        "edad": "17",
+                        "direccion": "10-call 3",
+                        "acudiente": "ana",
+                        "telefono": {
+                            "celular": 321467891,
+                            "fijo": 6782
+                        },
+                        "estado": "cursando",
+                        "riesgo": "bajo",
+                        "modulos": {}
+                    },
+                    {
+                        "id": "2",
+                        "nombre": "luis",
+                        "apellido": "caicedo",
+                        "edad": "19",
+                        "direccion": "13-call 6",
+                        "acudiente": "luis",
+                        "telefono": {
+                            "celular": 3124567894,
+                            "fijo": 889063
+                        },
+                        "estado": "cursando",
+                        "riesgo": "bajo",
+                        "modulos": {}
+                    }
+                ]
+            },
+            {
+                "ruta": "java",
+                "modulos": {
+                    "fundamentos de programacion": [
+                        "introduccion a la algoritmia",
+                        "PSeint",
+                        "Python"
+                    ],
+                    "programacion web": [
+                        "HTML",
+                        "CSS",
+                        "booststrap"
+                    ],
+                    "programacion formal": "java",
+                    "bases de datos": [
+                        "PostGresql",
+                        "Mysql"
+                    ],
+                    "backend": [
+                        "Spring Boot",
+                        "Expres"
+                    ]
+                },
+                "nombres": "t2",
+                "fecha de inicio": "10-02-2024",
+                "fecha de finalizacion": "10-11-2024",
+                "salon de entrenamiento": "aula2",
+                "grupo": [
+                    {
+                        "id": "3",
+                        "nombre": "pablo",
+                        "apellido": "contreras",
+                        "edad": "23",
+                        "direccion": "10-call4",
+                        "acudiente": "pedro",
+                        "telefono": {
+                            "celular": 312467832,
+                            "fijo": 6778
+                        },
+                        "estado": "cursando",
+                        "riesgo": "bajo",
+                        "modulos": {}
+                    },
+                    {
+                        "id": "4",
+                        "nombre": "maria",
+                        "apellido": "sandoval",
+                        "edad": "2",
+                        "direccion": "11-call6",
+                        "acudiente": "maria",
+                        "telefono": {
+                            "celular": 316783426,
+                            "fijo": 90876
+                        },
+                        "estado": "cursando",
+                        "riesgo": "bajo",
+                        "modulos": {}
+                    },
+                    {
+                        "id": "234",
+                        "nombre": "ZULLY",
+                        "apellido": "ortiz",
+                        "edad": "18",
+                        "direccion": "Diocesano",
+                        "acudiente": "Fabiola ",
+                        "telefono": {
+                            "celular": 31264846,
+                            "fijo": 12364
+                        },
+                        "estado": "cursando",
+                        "riesgo": "bajo",
+                        "modulos": {}
+                    }
+                ]
+            },
+            {
+                "ruta": "NetCore",
+                "modulos": {
+                    "fundamentos de programacion": [
+                        "introduccion a la algoritmia",
+                        "PSeint",
+                        "Python"
+                    ],
+                    "programacion web": [
+                        "HTML",
+                        "CSS",
+                        "booststrap"
+                    ],
+                    "programacion formal": "JavaScript",
+                    "bases de datos": [
+                        "PostGresql",
+                        "Mysql"
+                    ],
+                    "backend": [
+                        "NetCore",
+                        "Expres"
+                    ]
+                },
+                "nombres": "t3",
+                "fecha de inicio": "01-01-2024",
+                "fecha de finalizacion": "14-12-2024",
+                "salon de entrenamiento": "aula3",
+                "grupo": [
+                    {
+                        "id": "5",
+                        "nombre": "camilo",
+                        "apellido": "suarez",
+                        "edad": "18",
+                        "direccion": "14-call7",
+                        "acudiente": "camilo",
+                        "telefono": {
+                            "celular": 3214890,
+                            "fijo": 4321
+                        },
+                        "estado": "cursando",
+                        "riesgo": "bajo",
+                        "modulos": {
+                            "modulo1": "aprobado",
+                            "modulo2": "aprobado"
+                        }
+                    },
+                    {
+                        "id": "6",
+                        "nombre": "sofia",
+                        "apellido": "suarez",
+                        "edad": "19",
+                        "direccion": "1-call6",
+                        "acudiente": "sofia",
+                        "telefono": {
+                            "celular": 322478999,
+                            "fijo": 67843
+                        },
+                        "estado": "cursando",
+                        "riesgo": "alto",
+                        "modulos": {
+                            "modulo4": "aprobado",
+                            "modulo3": "aprobado",
+                            "modulo2": "desaprobado",
+                            "modulo5": "desaprobado",
+                            "modulo1": "desaprobado"
+                        }
+                    },
+                    {
+                        "id": "78",
+                        "nombre": "luz",
+                        "apellido": "perez",
+                        "edad": "18",
+                        "direccion": "20",
+                        "acudiente": "luz",
+                        "telefono": {
+                            "celular": 1244354,
+                            "fijo": 765752
+                        },
+                        "estado": "cursando",
+                        "riesgo": "",
+                        "modulos": {}
+                    }
+                ]
+            }
+        ],
+        "trainers": [
+            {
+                "id": 1,
+                "nombre": "pedro",
+                "apellido": "caceres",
+                "Telefono": 3212231223,
+                "user": "pedro",
+                "contrase\u00f1a": "p123",
+                "edad": "20",
+                "ruta": []
+            },
+            {
+                "id": 2,
+                "nombre": "michael",
+                "apellido": " rodriguez",
+                "Telefono": 3212256733,
+                "user": "michael",
+                "contrase\u00f1a": "m123",
+                "edad": "27",
+                "ruta": []
+            },
+            {
+                "id": 3,
+                "nombre": "orbin",
+                "apellido": "contreras",
+                "Telefono": 3212245623,
+                "user": "orbin",
+                "contrase\u00f1a": "o123",
+                "edad": "30",
+                "ruta": []
+            }
+        ],
+        "coordinador": [
+            {
+                "id": 123,
+                "nombre": "stiven",
+                "apellido": " carvajal",
+                "edad": "23",
+                "sede": "tibu"
+            }
+        ],
+        "ingreso": [
+            {
+                "id": "11",
+                "nombre": "jesus",
+                "apellido": "bustamante",
+                "edad": "17",
+                "direccion": "23-cal13",
+                "acudiente": "ingrid",
+                "telefono": {
+                    "celular": 324167823,
+                    "fijo": 33462
+                },
+                "estado": "progreso de ingreso",
+                "riesgo": "",
+                "modulos": {}
+            }
+        ],
+        "Registro_de_Entrada": [
+            {
+                "id": "234",
+                "fecha_entrada": "2024-08-15 16:50:39",
+                "actividades_realizadas": "una tarea",
+                "estado": "Inactiva"
+            }
+        ]
+    }
+]
+
+function menu(){//menu principal
+    console.log("");
+    console.log(":::::::::BIENBENIDO A NUESTRA PLATAFORMA::::::::\n"+
+                "1 MODULO DE COORDINACION ACADEMICA\n"+
+                "2 MODULO DE TRAINER\n"+
+                "3 MODULO DE REPORTE\n"+
+                "4 REGISTRAR ENTRADA\n"+
+                "5 SALIR\n");
+}
+function menucoordinador(){//submenu principal del coordinador
+    console.log("::::::::::MENÚ::::::::::\n"+
+                "1 listar estudiantes\n"+
+                "2 ingresar nuevos campers\n"+
+                "3 modulo de matriculas\n"+
+                "4 crear rutas academicas con los modulos\n"+
+                "5 asignar rutas de entrenamiento a los traniers\n"+
+                "6 notas de modulos\n"+
+                "7 consultar campers con riesgo alto\n"+
+                "8 actualizar datos de campers\n"+
+                "9 volver al menu principal\n"+
+                ":::::::::::::::::::::::::\n")
+}
+function menumodiftrainer(){//submenu de trainer para actualizaciones de datos
+    console.log(":::::::::: MENÚ DE DATOS ::::::::::\n"+
+                "1 Nombre\n"+
+                "2 Apellido\n"+
+                "3 Telefono\n"+
+                "4 User\n"+
+                "5 Contraseña\n"+
+                "6 Edad\n"+
+                "7 Finalizar\n"+
+                ":::::::::::::::::::::::::::::::::::\n")
+}
+function menutrainer(){//submenu principal del trainer
+    console.log("::::::::::::::MENÚ:::::::::::::\n"+
+                "1 mostrar horarios y ruta\n"+
+                "2 actualizar datos\n"+
+                "3 salir")
+}
+function menureportes(){//submenu para  el modulo de reporte 
+    console.log("::::::::::MENÚ::::::::::\n"+
+                "1 listar campers inscritos\n"+
+                "2 listar los campers que aprobaron el examen principal\n"+
+                "3 listar todos los entrenadores\n"+
+                "4 listar campers de bajo rendimiento\n"+
+                "5 listar campers y trainers que tienen ruta de entrenamiento\n"+
+                "6 mostrar cuantos campers perdieron y aprobaron cada uno de los modulos teniendo en cuenta la ruta de entrenamiento y el entrenador encargado\n"+
+                "7 volver al menu principal")
+}
+function menumodificaciones(){//submenu de modificaciones de datos de coordinador
+    console.log(":::::::::MENÚ MODIFICACIONES:::::::::::\n"+
+                "1 modificar nombre\n"+
+                "2 modificar apellido \n"+
+                "3 modificar edad \n"+
+                "4 modificar direccion\n"+
+                "5 modificar acudiente\n"+
+                "6 modificar numero de celular\n"+
+                "7 modificar numero fijo\n"+
+                "8 finalizar\n"+
+                ":::::::::::::::::::::::::::::::::::::\n");
+}
+function notasmodulos(){//submenu del coordinador para la asignacion de notas en cada modulo
+    console.log("------------MODULOS------------");
+    console.log("1 fundamentos de programacion");
+    console.log("2 programacion web");
+    console.log("3 programacion formal");
+    console.log("4 bases de datos");
+    console.log("5 backend");
+    console.log("6 finalizar");
+}
+
+var bool=true;
+while (bool==true){
+    menu()
+    var opc=prompt("ingrese su opcion por favor: ");
+    if (opc==1){
+        console.log("");
+        console.log("MODULO DE COORDINACION ACADEMICA");
+        console.log("");
+        menucoordinador();
+        var ver=true;
+        while (ver==true){
+            var opc1=prompt("ingrese su opcion por favor: ");
+            if (opc1==1){
+                for (const i of info[0]["grupos"]){
+                    console.log("----------------------");
+                    console.log(i["nombres"]);
+                    var cont = 0;
+                }
+                let punch=true
+                while (punch==true){
+                    let cell = 0;
+                    var gru = prompt("que grupo quieres listar");
+                    for (const i of info[0]["grupos"]){
+                        if (i["nombres"]==gru){
+                            cell=1;
+                            punch=false;
+                        }
+                    }
+                    if (cell==0){
+                        console.log("escoje un grupo de los que se mostraron (escribelo igual)");
+                    }
+                }
+                for (const i of info[0]["grupos"]){
+                    if (i["nombres"]==gru){
+                        for (const z of i["grupo"]){
+                            cont+=1
+                            console.log(" ")
+                            console.log("==================== CAMPER # ",cont,"==============================")
+                            console.log("id: ",z["id"])
+                            console.log("nombre: ", z["nombre"])
+                            console.log("apellido: ", z["apellido"])
+                            console.log("edad: ", z["edad"])
+                            console.log("direccion: ", z["direccion"])
+                            console.log("acudiente: ", z["acudiente"])
+                            console.log("telefono: ", z["telefono"])
+                            console.log("estado: ", z["estado"])
+                            console.log("riesgo: ", z["riesgo"])
+                            console.log("=========================================================")
+                            console.log("")
+                        }
+                    }
+                }
+                menucoordinador()//al finalizar lo anterior nos volvera a imprimir el menu de coordinador
+            }
+            else if (opc1==2){
+                bucle=true
+                while (bucle==true){
+                    console.log("");
+                    console.log(":::::::::AÑADIR NUEVO CAMPER:::::::::");
+                    
+                }
+            }
+            else if (opc1==9){
+                ver = false
+            }
+        }
+    }
+    else if (opc==5){
+        bool = false
+    }
+}
